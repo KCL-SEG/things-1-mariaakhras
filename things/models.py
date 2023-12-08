8b1f822e3fd0d1910d5c7814beebc9326dcc2a17
mariaakhras <mariaakhras055@gmail.com>
2023-12-08 11:37:33 +0000

    Add html file

--- things/models.py
+++ things/models.py
@@ -6,3 +6,8 @@ class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, blank=True)
     quantity = models.PositiveIntegerField(default=1, validators=[MaxValueValidator(100), MinValueValidator(0)])
+
+    def __str__(self):
+        return self.name
+    
+    
